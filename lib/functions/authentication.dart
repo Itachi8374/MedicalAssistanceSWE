b149bf82e1840b00434c4817c4bb03979848d2fa
Tanay Maheshwari <tanaymodani18@gmail.com>
2022-04-24 19:29:26 +0530

    added signup and login in authentication.dart

--- lib/functions/authentication.dart
+++ lib/functions/authentication.dart
@@ -14,3 +14,39 @@ Future<void> login({
     print(e.message!); // Displaying the error message
   }
 }
+
+// EMAIL SIGN UP
+  Future<void> signUpWithEmail({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      // if you want to display your own custom error message
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      print(e.message!); // Displaying the usual firebase error message
+    }
+  }
+
+  // EMAIL LOGIN
+  Future<void> loginWithEmail({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e.message!); // Displaying the error message
+    }
+  }
